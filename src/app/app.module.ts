7404355cfd098568e35a60409a7ba6e1ad4cdc3e
Maria <mar1asm@yahoo.com>
2021-03-02 18:01:11 +0200

    new folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { PageComponent } from './model/page/page.component';
+import { PageComponent } from './maria/page/page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
